ce4679048464a180be80397692caf0803c76d515
andreiolivar <andrei.olivar78@gmail.com>
2026-02-25 10:24:52 +1100

    complete part 2

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -1,23 +1,29 @@
 import express, { Request, Response } from "express";
+import z from "zod";
 
 // ==== Type Definitions, feel free to add or modify ==========================
-interface cookbookEntry {
-  name: string;
-  type: string;
-}
+const requiredItemSchema = z.object({
+  name: z.string(),
+  quantity: z.number().min(0)
+});
 
-interface requiredItem {
-  name: string;
-  quantity: number;
-}
+const recipeSchema = z.object({
+  type: z.literal("recipe"),
+  name: z.string(),
+  requiredItems: z.array(requiredItemSchema)
+});
 
-interface recipe extends cookbookEntry {
-  requiredItems: requiredItem[];
-}
+const ingredientSchema = z.object({
+  type: z.literal("ingredient"),
+  name: z.string(),
+  cookTime: z.number().min(0, { error: "cookTime must be greater than or equal to 0" })
+});
 
-interface ingredient extends cookbookEntry {
-  cookTime: number;
-}
+const cookbookEntrySchema = z.discriminatedUnion("type", [recipeSchema, ingredientSchema]);
+
+const cookbookSchema = z.array(cookbookEntrySchema);
+type Cookbook = z.infer<typeof cookbookSchema>;
+
 
 type TextTransformer = (text: string) => string;
 
@@ -28,7 +34,7 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: Cookbook = [];
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -74,9 +80,30 @@ const parse_handwriting = (recipeName: string): string | null => {
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const parsed = cookbookEntrySchema.safeParse(req.body);
 
+  if (!parsed.success) {
+    res.status(400).json(z.treeifyError(parsed.error));
+  }
+
+  const data = parsed.data;
+
+  if (cookbook.find(entry => entry.name === data.name) !== undefined) {
+    res.status(400).json({ message: "Entry name already exists." });
+  }
+
+  if (data.type === "recipe") {
+    const names = data.requiredItems.map(entry => entry.name);
+    const namesSet = new Set(names);
+
+    if (names.length !== namesSet.size) {
+      res.status(400).json({ message: "Required items can only have one element per name" });
+    }
+  }
+
+  cookbook.push(data);
+
+  res.status(200).json({});
 });
 
 // [TASK 3] ====================================================================
