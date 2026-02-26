23b3f2cedd1d54fff73863e9753fed273a81df3c
andreiolivar <andrei.olivar78@gmail.com>
2026-02-26 15:55:35 +1100

    frontend: complete navbar

--- frontend/frontend-task/src/App.tsx
+++ frontend/frontend-task/src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar/Navbar'
+import Searchbar from './components/Searchbar/Searchbar'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Searchbar/>
     </>
   )
 }
---
+++ frontend/frontend-task/src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,28 @@
+import styles from "./Searchbar.module.css"
+
+export default function Searchbar() {
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-funnel-fill" viewBox="0 0 16 16">
+          <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"/>
+        </svg>
+        <span>Filters</span>
+      </button>
+
+    <div className={styles.searchContainer}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+      </svg>
+      <input className={styles.searchbar} type="text" placeholder="Search for a building..." />
+    </div>
+    
+      <button className={styles.button}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-filter" viewBox="0 0 16 16">
+          <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
+        </svg>
+        <span>Sort</span>
+      </button>
+    </div>
+  )
+}
